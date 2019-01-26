5c45717f235843fb4857f6094157cc14af01d261
Lucy <lucy13939@gmail.com>
2019-01-26 15:02:02 -0800

    Deleted random files

--- server.py
+++ server.py
@@ -3,22 +3,10 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-	return render_template('homepage.html')
 
 @app.route("/resume")
 def resume():
-	return render_template('resume2.html')
-
-#@app.route('/sentiment_post')
-	#make a request to get data from redit
-	#run sentiment analyzer using code
-	#pass in sentiment ane current post to our html
+	return render_template('resume.html')
 
 if __name__ == "__main__":
 	app.run()
-
-@app.route("/sent_anl")
-def sentiment_analyzer():
-	return render_template('sent_anl.html')
